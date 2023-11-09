e760206c1cc79cda80a6eb9d80bc7f0531b68336
brunaogomes11 <brunogomesper@gmail.com>
2023-11-09 13:10:23 -0300

    Mudança 2.1

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 function calcular2_1(E, Q, d) {
     equacao_padrao = `\\[E = \\frac{K \\cdot Q}{d^2}\\]`
+    document.querySelector(".equacao_usada").innerHTML = equacao_padrao
     K = (8.99)*(Math.pow(10,9))
     if (E != null && Q != null && d == null) {
         d = Math.sqrt((K*Q)/E)
-        document.querySelector(".equacao_usada").innerHTML = equacao_padrao
         document.querySelector(".equacao_usada").innerHTML += `\\[d = \\sqrt{\\frac{K \\cdot Q}{E}}\\]`
         document.querySelector(".equacao_usada").innerHTML += `\\[d = \\sqrt{\\frac{8.99 \\cdot 10^9 \\cdot ${Q}}{${E}}}\\]`
         document.querySelector(".equacao_usada").innerHTML += `\\[d = ${converterParaNotacao10x(d)} N/m\\]`
     } else if (E == null && Q != null && d != null) {
         E = (K*Q)/(Math.pow(d, 2))
-        document.querySelector(".equacao_usada").innerHTML = equacao_padrao
         document.querySelector(".equacao_usada").innerHTML += `\\[E = \\frac{8.99 \\cdot 10^9 \\cdot ${Q}}{${d}^2}\\]`
         document.querySelector(".equacao_usada").innerHTML += `\\[E = \\frac{${K*Q}}{${Math.pow(d,2)}}\\]`
         document.querySelector(".equacao_usada").innerHTML += `\\[E = ${converterParaNotacao10x(E)} N/m\\]`
     } else if (E != null && Q == null && d != null) {
         Q = (E*(Math.pow(d, 2))/K)
-        document.querySelector(".equacao_usada").innerHTML = equacao_padrao
         document.querySelector(".equacao_usada").innerHTML += `\\[Q = \\frac{E \\cdot d^2}{8.99 \\cdot 10^9}\\]`
         document.querySelector(".equacao_usada").innerHTML += `\\[Q = \\frac{${E} \\cdot ${d}^2}{8.99 \\cdot 10^9}\\]`
         document.querySelector(".equacao_usada").innerHTML += `\\[Q = ${converterParaNotacao10x(Q)} N/m\\]`
