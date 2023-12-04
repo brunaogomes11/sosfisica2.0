59d80f2ae585f92053c9d433a805c772ce56186f
brunaogomes11 <brunogomesper@gmail.com>
2023-12-03 22:00:00 -0300

    terminando de adicionar conversores

--- index.js
+++ index.js
@@ -3,9 +3,25 @@ document.querySelector("#selecionar_variavel_achar").addEventListener("change", 
     if (selecao == "E") {
         document.querySelector("#campos_entrada1").innerHTML = `
             <span>Q - Carga elétrica</span>
-            <input class="entradaNumeros" id="entrada_q" name="entrada_q" type="number" start=0 />
+            <div class="entradasContainer">
+                <input class="entradaNumeros" id="entrada_q" name="entrada_q" type="number" start=0 />
+                <select class="tipoInput" id="tipo_entrada_q1_s1">
+                    <option value="C">C</option>
+                    <option value="mC">mC</option>
+                    <option value="µC">µC</option>
+                    <option value="nC">nC</option>
+                    <option value="pC">pC</option>
+                </select>
+            </div>
             <span>d - Distância</span>
-            <input class="entradaNumeros" id="entrada_d" name="entrada_d" type="number" start=0 />
+            <div class="entradasContainer">
+                    <input class="entradaNumeros" id="entrada_d" name="entrada_d" type="number" start=0 />
+                    <select class="tipoInput" id="tipo_entrada_d_s1">
+                        <option value="m">m</option>
+                        <option value="cm">cm</option>
+                        <option value="mm">mm</option>
+                    </select>
+                </div> 
             <div class="buttons" onclick="calcular2_1('E')">Calcular</div>
         `
     } else if (selecao == "Q") {
@@ -13,7 +29,14 @@ document.querySelector("#selecionar_variavel_achar").addEventListener("change", 
             <span>E - Campo elétrico</span>
             <input class="entradaNumeros" id="entrada_E" name="entrada_E" type="number" start=0 />
             <span>d - Distância</span>
-            <input class="entradaNumeros" id="entrada_d" name="entrada_d" type="number" start=0 />
+            <div class="entradasContainer">
+                <input class="entradaNumeros" id="entrada_d" name="entrada_d" type="number" start=0 />
+                <select class="tipoInput" id="tipo_entrada_d_s1">
+                    <option value="m">m</option>
+                    <option value="cm">cm</option>
+                    <option value="mm">mm</option>
+                </select>
+            </div> 
             <div class="buttons" onclick="calcular2_1('Q')">Calcular</div>
         `
     } else if (selecao == "d") {
@@ -21,7 +44,16 @@ document.querySelector("#selecionar_variavel_achar").addEventListener("change", 
             <span>E - Campo elétrico</span>
             <input class="entradaNumeros" id="entrada_E" name="entrada_E" type="number" start=0 />
             <span>Q - Carga elétrica</span>
-            <input class="entradaNumeros" id="entrada_q" name="entrada_q" type="number" start=0 />
+            <div class="entradasContainer">
+                <input class="entradaNumeros" id="entrada_q" name="entrada_q" type="number" start=0 />
+                <select class="tipoInput" id="tipo_entrada_q1_s1">
+                    <option value="C">C</option>
+                    <option value="mC">mC</option>
+                    <option value="µC">µC</option>
+                    <option value="nC">nC</option>
+                    <option value="pC">pC</option>
+                </select>
+            </div>
             <div class="buttons" onclick="calcular2_1('d')">Calcular</div>
         `
     } else {
@@ -34,13 +66,13 @@ function calcular2_1(selecao) {
     let Q = null
     let d = null
     if(selecao == "E") {
-        Q = document.querySelector("#entrada_q").value
-        d = document.querySelector("#entrada_d").value
+        Q = document.querySelector("#tipo_entrada_q1_s1").value == 'C' ? parseFloat(document.querySelector("#entrada_q").value) : conversorCoulomb('C', document.querySelector("#tipo_entrada_q1_s1").value, document.querySelector("#entrada_q").value)
+        d = document.querySelector("#tipo_entrada_d_s1").value == 'm' ? parseFloat(document.querySelector("#entrada_d").value) : conversorDistancia('m', document.querySelector("#tipo_entrada_d_s1").value, document.querySelector("#entrada_d").value)
     } else if(selecao == "Q") {
         E = document.querySelector("#entrada_E").value
-        d = document.querySelector("#entrada_d").value
+        d = document.querySelector("#tipo_entrada_d_s1").value == 'm' ? parseFloat(document.querySelector("#entrada_d").value) : conversorDistancia('m', document.querySelector("#tipo_entrada_d_s1").value, document.querySelector("#entrada_d").value)
     } else if(selecao == "d") {
-        Q = document.querySelector("#entrada_q").value
+        Q = document.querySelector("#tipo_entrada_q1_s1").value == 'C' ? parseFloat(document.querySelector("#entrada_q").value) : conversorCoulomb('C', document.querySelector("#tipo_entrada_q1_s1").value, document.querySelector("#entrada_q").value)
         E = document.querySelector("#entrada_E").value
     }
     equacao_padrao = `\\[E = \\frac{K \\cdot Q}{d^2}\\]`
@@ -48,14 +80,15 @@ function calcular2_1(selecao) {
     if (E != '' && Q != '' && d == null && Math.sign(Q) == Math.sign(E)) {
         d = Math.sqrt((K*Q)/E)
         document.querySelector(".resultado_completo").innerHTML += `\\[d = \\sqrt{\\frac{K \\cdot Q}{E}}\\]`
-        document.querySelector(".resultado_completo").innerHTML += `\\[d = \\sqrt{\\frac{8.99 \\cdot 10^9 \\cdot ${Q}}{${E}}}\\]`
+        document.querySelector(".resultado_completo").innerHTML += `\\[d = \\sqrt{\\frac{8.99 \\cdot 10^9 \\cdot ${converterParaNotacao10x(Q)}}{${E}}}\\]`
+        document.querySelector(".resultado_completo").innerHTML += `\\[d = \\sqrt{\\frac{${converterParaNotacao10x(K*Q)}}{${E}}}\\]`
         document.querySelector(".resultado_completo").innerHTML += `\\[d = ${converterParaNotacao10x(d)} N/m\\]`
         document.querySelector(".resultado_resumido").innerHTML = `\\[d = ${converterParaNotacao10x(d)} N/m\\]`
         document.querySelector(".resultado_resumido").innerHTML += `<div id="mostrarButton" class="buttons" onclick="mostrarCalculos()">Mostrar Cálculos</div>`
     } else if (E == null && Q != '' && d != '') {
         E = (K*Q)/(Math.pow(d, 2))
-        document.querySelector(".resultado_completo").innerHTML += `\\[E = \\frac{8.99 \\cdot 10^9 \\cdot ${Q}}{${d}^2}\\]`
-        document.querySelector(".resultado_completo").innerHTML += `\\[E = \\frac{${K*Q}}{${Math.pow(d,2)}}\\]`
+        document.querySelector(".resultado_completo").innerHTML += `\\[E = \\frac{8.99 \\cdot 10^9 \\cdot ${converterParaNotacao10x(Q)}}{${d}^2}\\]`
+        document.querySelector(".resultado_completo").innerHTML += `\\[E = \\frac{${converterParaNotacao10x(K*Q)}}{${Math.pow(d,2)}}\\]`
         document.querySelector(".resultado_completo").innerHTML += `\\[E = ${converterParaNotacao10x(E)} N/m\\]`
         document.querySelector(".resultado_resumido").innerHTML = `\\[E = ${converterParaNotacao10x(E)} N/m\\]`
         document.querySelector(".resultado_resumido").innerHTML += `<div id="mostrarButton" class="buttons" onclick="mostrarCalculos()">Mostrar Cálculos</div>`
@@ -131,10 +164,10 @@ function calcular2_3() {
 }
 
 function calcular2_4() {
-    let q1 = parseFloat(document.querySelector("#entrada_q1_s4").value)
-    let q2 = parseFloat(document.querySelector("#entrada_q2_s4").value)
-    let q3 = parseFloat(document.querySelector("#entrada_q3_s4").value)
-    let d = parseFloat(document.querySelector("#entrada_d_s4").value)
+    let q1 = document.querySelector("#tipo_entrada_q1_s4").value == 'C' ? parseFloat(document.querySelector("#entrada_q1_s4").value) : conversorCoulomb('C', document.querySelector("#tipo_entrada_q1_s4").value, document.querySelector("#entrada_q1_s4").value)
+    let q2 = document.querySelector("#tipo_entrada_q2_s4").value == 'C' ? parseFloat(document.querySelector("#entrada_q2_s4").value) : conversorCoulomb('C', document.querySelector("#tipo_entrada_q2_s4").value, document.querySelector("#entrada_q2_s4").value)
+    let q3 = document.querySelector("#tipo_entrada_q3_s4").value == 'C' ? parseFloat(document.querySelector("#entrada_q3_s4").value) : conversorCoulomb('C', document.querySelector("#tipo_entrada_q3_s4").value, document.querySelector("#entrada_q3_s4").value)
+    let d = document.querySelector("#tipo_entrada_d_s4").value == 'm' ? parseFloat(document.querySelector("#entrada_d_s4").value) : conversorDistancia('m', document.querySelector("#tipo_entrada_d_s4").value, document.querySelector("#entrada_d_s4").value)
     equacao_p_total = `\\[\\overrightarrow{E_p} = \\overrightarrow{E_1}+\\overrightarrow{E_2}+\\overrightarrow{E_3}\\]`
     eq1 = `\\[\\overrightarrow{E_{q_1}} = K \\cdot \\frac{q_1}{d^2} \\cdot (-î)\\]`
     eq2 = `\\[\\overrightarrow{E_{q_2}} = K \\cdot \\frac{q_2}{2d^2} (\\frac{-\\sqrt{2}}{2}î - \\frac{-\\sqrt{2}}{2}ĵ)\\]`
